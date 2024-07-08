ead3d15523847679fbd7d9ab9494644de4f84fea
jameslam <james@tecky.io>
2024-07-08 16:46:58 +0800

    transformer train

--- test.py
+++ test.py
@@ -1,47 +1,51 @@
-import numpy as np
-import pandas as pd
+import os
+
 import tensorflow as tf
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
+from tensorflow import keras
+import pathlib                               
+import matplotlib.pylab as plt
+import cv2
+import numpy as np
+import threading
+from transformers import ViTForImageClassification
 
-## columns name of the dataset
-feature_names = [
-    'gdp',
-    'total_population',
-    'inflation_rate',
-    'working_age',
-    'immigrant_population',
-]
-label_name = 'average_property_price_of_housing_estates'
-column_names = feature_names + [label_name]
 
-df = pd.read_csv('./dataset/property_prices.csv', 
-                 skiprows=1,
-                 usecols=[1,2,3,4,5,6],
-                 names=column_names)
-y = df['average_property_price_of_housing_estates']
-df = df.drop(['average_property_price_of_housing_estates'], axis=1)
-X = df[feature_names]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-# Create and train the Linear Regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
+def display_image(resize_img):
+    plt.figure()
+    plt.imshow(resize_img)
+    plt.colorbar()
+    plt.grid(True)
+    plt.xlabel(class_names[0])
+    plt.show()
+    
+print(tf.version.VERSION)
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
+probability_model = tf.keras.models.load_model('my_model.h5')
 
-# Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+class_names = ['cats', 'dogs']
+test_dir=pathlib.Path("./dog_cat_dataset/testing_set")
+cat_test= list(test_dir.glob('cats/*'))
+dog_test= list(test_dir.glob('dogs/*'))
 
-print("Mean Squared Error:", mse)
-print("R-squared:", r2)
+predict_index = 45
+# Grab an image from the test dataset.
+image = cat_test[predict_index]
+img = cv2.imread(str(image))
 
-# Use the model to make a prediction
-# new_data = np.array([[28.7, 90, 1010.3, 10.1]])
-new_data = pd.DataFrame([[3598.39, 7.41, 1.88, 5.16,2]], columns=feature_names)
+resize_img = cv2.resize(img, (224, 224))
+resize_img = resize_img / 255.0
 
-rainfall_prediction = model.predict(new_data)
-print("Predicted rainfall:", rainfall_prediction[0])
+threading.Thread(target=display_image(resize_img), daemon=True).start()
+
+
+# Add the image to a batch where it's the only member.
+resize_img = (np.expand_dims(resize_img,0))
+
+print(resize_img.shape)
+
+predictions_single = probability_model.predict(resize_img)
+predictions_label = class_names[np.argmax(predictions_single[0])]
+
+print(predictions_single)
+print(predictions_label)
+
